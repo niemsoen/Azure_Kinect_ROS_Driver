793cb14bc7851f2cf22dc475ee21cf82ce8c26e5
Sönke Niemann <soenke.niemann@ipk.fraunhofer.de>
2022-05-16 12:19:50 +0200

    typo

--- install-dependencies_auto-dist.sh
+++ install-dependencies_auto-dist.sh
@@ -29,12 +29,12 @@ if [[ $os = "focal" ]]; then
   elif [[ $arch = "aarch64" ]]; then
     if [[ "$(dpkg -s libk4a1.4-dev | grep Status | awk '{ print $4 }')" != "installed" ]]; then
       echo "Retrieving azure kinect packages for architecture $arch. Be patient..."
-      curl -sSL https://packages.microsoft.com/ubuntu/18.04/multiarch/prod/pool/main/libk/libk4a1.4/libk4a1.4_1.4.1_amd64.deb > /tmp/libk4a1.4_1.4.1_amd64.deb
-      curl -sSL https://packages.microsoft.com/ubuntu/18.04/multiarch/prod/pool/main/libk/libk4a1.4-dev/libk4a1.4-dev_1.4.1_amd64.deb > /tmp/libk4a1.4-dev_1.4.1_amd64.deb
+      curl -sSL https://packages.microsoft.com/ubuntu/18.04/multiarch/prod/pool/main/libk/libk4a1.4/libk4a1.4_1.4.1_arm64.deb > /tmp/libk4a1.4_1.4.1_arm64.deb
+      curl -sSL https://packages.microsoft.com/ubuntu/18.04/multiarch/prod/pool/main/libk/libk4a1.4-dev/libk4a1.4-dev_1.4.1_arm64.deb > /tmp/libk4a1.4-dev_1.4.1_arm64.deb
       echo 'libk4a1.4 libk4a1.4/accepted-eula-hash string 0f5d5c5de396e4fee4c0753a21fee0c1ed726cf0316204edda484f08cb266d76' | sudo debconf-set-selections
       echo 'libk4a1.4 libk4a1.4/accept-eula select true' | sudo debconf-set-selections
-      sudo dpkg -i /tmp/libk4a1.4_1.4.1_amd64.deb
-      sudo dpkg -i /tmp/libk4a1.4-dev_1.4.1_amd64.deb
+      sudo dpkg -i /tmp/libk4a1.4_1.4.1_arm64.deb
+      sudo dpkg -i /tmp/libk4a1.4-dev_1.4.1_arm64.deb
     fi
   
   else
