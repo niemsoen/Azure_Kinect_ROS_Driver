54ab55cdb7660492899a00681918a1037125df19
niemsoen <soenke.niemann@googlemail.com>
2022-07-21 11:38:50 +0200

    Driver without urdf joints (#2)
    * add vanilla driver launchfile and adapt readme
    
    * need to decide on dependency handling
    
    * better dependency install output
    
    * remove architecture-specific install scripts - auto script working
    
    * add exit code to script
    
    Co-authored-by: Sönke Niemann <soenke.niemann@ipk.fraunhofer.de>

--- install-dependencies_auto-dist.sh
+++ install-dependencies_auto-dist.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # https://github.com/microsoft/Azure-Kinect-Sensor-SDK/issues/1263#issuecomment-983804473
 
+echo "--------- Azure Kinect Camera Driver: Dependencies"
+
 hash lsb_release 2>/dev/null || { echo "ERROR: tool lsb_release needed for analysis"; exit 1; }
 os=$(lsb_release -cs)
 arch=$(uname -m)
@@ -22,8 +24,8 @@ if [[ $os = "focal" ]]; then
       curl -sSL https://packages.microsoft.com/ubuntu/18.04/prod/pool/main/libk/libk4a1.4-dev/libk4a1.4-dev_1.4.1_amd64.deb > /tmp/libk4a1.4-dev_1.4.1_amd64.deb
       echo 'libk4a1.4 libk4a1.4/accepted-eula-hash string 0f5d5c5de396e4fee4c0753a21fee0c1ed726cf0316204edda484f08cb266d76' | sudo debconf-set-selections
       echo 'libk4a1.4 libk4a1.4/accept-eula select true' | sudo debconf-set-selections
-      sudo dpkg -i /tmp/libk4a1.4_1.4.1_amd64.deb
-      sudo dpkg -i /tmp/libk4a1.4-dev_1.4.1_amd64.deb
+      sudo dpkg -i /tmp/libk4a1.4_1.4.1_amd64.deb || { echo "dpkg install failed"; exit 1; }
+      sudo dpkg -i /tmp/libk4a1.4-dev_1.4.1_amd64.deb || { echo "dpkg install failed"; exit 1; }
     else
       echo "--------- Dependencies already satisfied"
     fi
@@ -35,15 +37,15 @@ if [[ $os = "focal" ]]; then
       curl -sSL https://packages.microsoft.com/ubuntu/18.04/multiarch/prod/pool/main/libk/libk4a1.4-dev/libk4a1.4-dev_1.4.1_arm64.deb > /tmp/libk4a1.4-dev_1.4.1_arm64.deb
       echo 'libk4a1.4 libk4a1.4/accepted-eula-hash string 0f5d5c5de396e4fee4c0753a21fee0c1ed726cf0316204edda484f08cb266d76' | sudo debconf-set-selections
       echo 'libk4a1.4 libk4a1.4/accept-eula select true' | sudo debconf-set-selections
-      sudo dpkg -i /tmp/libk4a1.4_1.4.1_arm64.deb
-      sudo dpkg -i /tmp/libk4a1.4-dev_1.4.1_arm64.deb
+      sudo dpkg -i /tmp/libk4a1.4_1.4.1_arm64.deb || { echo "dpkg install failed"; exit 1; }
+      sudo dpkg -i /tmp/libk4a1.4-dev_1.4.1_arm64.deb || { echo "dpkg install failed"; exit 1; }
     else
       echo "--------- Dependencies already satisfied"
     fi
   
   else
     echo "--------- Sorry, architecture $arch not supported"
-    exit
+    exit 1
   fi
 
 elif [[ $arch = "bionic" ]]; then
@@ -55,12 +57,13 @@ elif [[ $arch = "bionic" ]]; then
   sudo apt-get update
   echo 'libk4a1.4 libk4a1.4/accepted-eula-hash string 0f5d5c5de396e4fee4c0753a21fee0c1ed726cf0316204edda484f08cb266d76' | sudo debconf-set-selections
   echo 'libk4a1.4 libk4a1.4/accept-eula select true' | sudo debconf-set-selections
-  sudo apt-get install -y --no-install-recommends libk4a1.4-dev
+  sudo apt-get install -y --no-install-recommends libk4a1.4-dev || { echo "apt install failed"; exit 1; }
 else
   echo "--------- Sorry, no supported OS detected"
-  exit
+  exit 1
 fi
 
 echo "--------- Setting up udev rules"
 curl -s https://raw.githubusercontent.com/microsoft/Azure-Kinect-Sensor-SDK/develop/scripts/99-k4a.rules >> 99-k4a.rules
 sudo mv 99-k4a.rules /etc/udev/rules.d/
+exit 0
