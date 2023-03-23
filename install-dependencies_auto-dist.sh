2b7b18040207795877f3407465cc092b514f1184
carolinaschorn <44744043+carolinaschorn@users.noreply.github.com>
2023-03-23 17:53:33 +0100

    Humble (#4)
    * galactic to humble
    
    * change readme install to humble
    
    * update readme to humble
    
    * edit settings of rviz. remove depth to make it work
    
    ---------
    
    Co-authored-by: Carolina Schorn <carolina.sophie.schorn@ipk.fraunhofer.de>

--- install-dependencies_auto-dist.sh
+++ install-dependencies_auto-dist.sh
@@ -8,7 +8,36 @@ os=$(lsb_release -cs)
 arch=$(uname -m)
 echo "--------- $arch detected"
 
-if [[ $os = "focal" ]]; then
+  
+if [[ $os = "jammy" ]]; then
+  echo "--------- Ubuntu 22.04 jammy detected"
+  
+  if [[ $arch = "x86_64" ]]; then
+    # Special hack for Azure Kinect Framework not available on Ubuntu 22.04
+    # Install k4a SDK from their repository.
+    # Bit of a hack, see https://github.com/microsoft/Azure-Kinect-Sensor-SDK/issues/1263
+    # The simple solution by @vinesmsuic does not seem to work. This uses the manual 
+    # solution by @atinfinity
+    # Also, https://github.com/microsoft/Azure-Kinect-Sensor-SDK/issues/1190 is part of this solution (needed to non-interactively accept EULA)
+    if [[ "$(dpkg -s libk4a1.4-dev | grep Status | awk '{ print $4 }')" != "installed" ]]; then
+      echo "Retrieving azure kinect packages for architecture $arch. Be patient..."
+      curl -sSL https://packages.microsoft.com/ubuntu/18.04/prod/pool/main/libk/libk4a1.4/libk4a1.4_1.4.1_amd64.deb > /tmp/libk4a1.4_1.4.1_amd64.deb
+      curl -sSL https://packages.microsoft.com/ubuntu/18.04/prod/pool/main/libk/libk4a1.4-dev/libk4a1.4-dev_1.4.1_amd64.deb > /tmp/libk4a1.4-dev_1.4.1_amd64.deb
+      echo 'libk4a1.4 libk4a1.4/accepted-eula-hash string 0f5d5c5de396e4fee4c0753a21fee0c1ed726cf0316204edda484f08cb266d76' | sudo debconf-set-selections
+      echo 'libk4a1.4 libk4a1.4/accept-eula select true' | sudo debconf-set-selections
+      sudo dpkg -i /tmp/libk4a1.4_1.4.1_amd64.deb || { echo "dpkg install failed"; exit 1; }
+      sudo dpkg -i /tmp/libk4a1.4-dev_1.4.1_amd64.deb || { echo "dpkg install failed"; exit 1; }
+      sudo apt install libsoundio-dev #this added line is the only thing different from focal to humble
+    else
+      echo "--------- Dependencies already satisfied"
+    fi
+
+  else
+    echo "--------- Sorry, architecture $arch not supported"
+    exit 1
+  fi
+
+elif [[ $os = "focal" ]]; then
   echo "--------- Ubuntu 20.04 focal detected"
   
   if [[ $arch = "x86_64" ]]; then
@@ -29,7 +58,7 @@ if [[ $os = "focal" ]]; then
     else
       echo "--------- Dependencies already satisfied"
     fi
-  
+
   elif [[ $arch = "aarch64" ]]; then
     if [[ "$(dpkg -s libk4a1.4-dev | grep Status | awk '{ print $4 }')" != "installed" ]]; then
       echo "Retrieving azure kinect packages for architecture $arch. Be patient..."
